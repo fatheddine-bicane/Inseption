af5afcf4360f7c22f5f55d23273e52595f6f8eb0
fatheddine-bicane <fatheddine04@gmail.com>
2026-07-16 20:21:13 +0100

    fix: the db itializing script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,12 +2,23 @@
 
 DB_PASSWORD=$(cat /run/secrets/db_password)
 
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
 if [ ! -d "/var/lib/mysql/$DATA_BASE" ]; then
-    service mariadb start
-    mariadb -e "CREATE DATABASE $DATA_BASE;"
-    mariadb -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-    mariadb -e "GRANT ALL PRIVILEGES ON $DATA_BASE.* TO '$DB_USER';"
-    service mariadb stop
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	service mariadb start
+
+	sleep 2
+
+	mariadb -e "CREATE DATABASE IF NOT EXISTS $DATA_BASE;"
+	mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+
+	# 2. Correctly assign privileges to the specific remote user profile
+	mariadb -e "GRANT ALL PRIVILEGES ON $DATA_BASE.* TO '$DB_USER'@'%';"
+	mariadb -e "FLUSH PRIVILEGES;"
+
+	service mariadb stop
 fi
 
-exec mariadbd
+exec mariadbd --user=mysql
