5c6ba9332f296013b53376fefe79e09033271c4c
fatheddine-bicane <fatheddine04@gmail.com>
2026-07-16 20:12:16 +0100

    fix: script to start the wordpress service

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -4,23 +4,24 @@ WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_REGULAR_PASSWORD=$(cat /run/secrets/wp_regular_password)
 DB_PASSWORD=$(cat /run/secrets/db_password)
 
-
 cd /var/www/wordpress
 
-sleep 10
-
-if [ ! -f "wp-settings.php" ]; then
-    echo "Downloading WordPress..."
-
-	#download wp
+#download the wp-cli
+if [ ! -f "/usr/local/bin/wp" ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
+fi
 
-    #download core files
-    wp core download  --allow-root
+#block execution until the MariaDB socket is fully accepting TCP connections
+until mariadb -h mariadb_1 -u ${DB_USER} -p${DB_PASSWORD} -e "SELECT 1;" > /dev/null 2>&1; do
+    echo "waiting for MariaDB to initialize and open port 3306..."
+    sleep 2
+done
 
-	#configure wp
+#check for config file
+if [ ! -f "wp-config.php" ]; then
+    wp core download --allow-root
     wp config create \
         --dbname=${DATA_BASE} \
         --dbuser=${DB_USER} \
@@ -28,20 +29,23 @@ if [ ! -f "wp-settings.php" ]; then
         --dbhost=mariadb_1 \
         --allow-root
 
-	wp core install \
-        --url=${DOMAIN_NAME} \
+    wp core install \
+        --url=https://${DOMAIN_NAME} \
         --title="Inception" \
         --admin_user=${WP_ADMIN_USER} \
         --admin_password=${WP_ADMIN_PASSWORD} \
         --admin_email=${WP_ADMIN_EMAIL} \
         --allow-root
 
-	wp user create \
+    wp user create \
         ${WP_REGULAR_USER} \
         ${WP_REGULAR_EMAIL} \
         --role=author \
-        --user_pass=${WP_USER_PASSWORD} \
+        --user_pass=${WP_REGULAR_PASSWORD} \
         --allow-root
 fi
 
-exec php-fpm8.4 -F
+#ensure correct permissions for nginx to read the populated files
+chown -R www-data:www-data /var/www/wordpress
+
+exec /usr/sbin/php-fpm8.4 -F
